85e89ef1920df2c6be40d6b21538aaa8ddc5a74b
Nicholas Engleman <nicholas@englemaninvest.com>
2016-07-13 18:12:20 +0800

    release 1.02

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Nicholas on 7/10/2016.
+ */
+$(document).ready(function() {
+
+    var startPos;
+    
+    var geoSuccess = function(position) {
+        startPos = position;
+        alert("Your Latitude is " + startPos.coords.latitude + " and your longitude is " + startPos.coords.longitude);
+        };
+    navigator.geolocation.getCurrentPosition();
+
+
+});
