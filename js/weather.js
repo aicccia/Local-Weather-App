0b90f8eda209a5368b7ed05c57fdc53e5db5efe6
Nicholas Engleman <nicholas@englemaninvest.com>
2018-01-27 23:56:09 -0500

    add input box so the weather at any zipcode can be checked

--- js/weather.js
+++ js/weather.js
@@ -1,36 +1,47 @@
-/**
- * Created by Nicholas on 7/10/2016.
- */
+function getCurrentLatLog(position) {
+	getWeather(position.coords.latitude, position.coords.longitude);
+}
 
-$(document).ready(function() {
+function getWeather(lat, log) {
+	const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=4bd0296ac3468ba55671920cabb0f745`;
 
-	navigator.geolocation.getCurrentPosition(getWeatherData);
+	$.getJSON(url, function (weather) {
+		const temp = weather.main.temp;
+		const tempConverted = (Math.floor(temp) * (9 / 5) - 459.67).toFixed(1);
+		$(".temperature").empty().append(`<p> ${tempConverted} F</p>`);
+		const city = weather.name;
+		const country = weather.sys.country;
+		$(".location").empty().append(`<p> ${city}, ${country} </p>`);
+		const des = weather.weather[0].description;
+		$(".description").empty().append(`<p> ${des} </p>`);
+		let windspeed = weather.wind.speed;
+		windspeed = ((windspeed * 3600) * 3.28 / 5280).toFixed(0);
+		$(".wind").empty().append(`<p>Wind is blowing at ${windspeed} mph.</p>`);
+		const hum = weather.main.humidity;
+		$(".sky").empty().append("<p>Humidity is " + hum + "%.</p>");
+	});
+}
 
-	function getWeatherData(position) {
-		const lat = position.coords.latitude;
-		const log = position.coords.longitude;
+$(document).ready(function () {
+	navigator.geolocation.getCurrentPosition(getCurrentLatLog);
 
-		const url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + log + "&appid=4bd0296ac3468ba55671920cabb0f745";
+	$("#zipcode").submit(function (event) {
+		const zipinput = $("#zip").val();
+		//verifies that the input is a 5-digit number
+		if ((/^\d{5}$/g.test(zipinput))) {
+			const zipurl = `http://maps.googleapis.com/maps/api/geocode/json?address=${zipinput}`;
+			$.getJSON(zipurl, function (zipdata) {
+				let newlat = zipdata.results[0].geometry.location.lat;
+				let newlog = zipdata.results[0].geometry.location.lng;
 
-		$.getJSON(url, function(weather) {
-			const temp = weather.main.temp;
-			const tempConverted = (Math.floor(temp)*(9/5)-459.67).toFixed(1);
-			$(".temperature").empty().append(`<p> ${tempConverted} F</p>`);
-
-			const city = weather.name;
-			const country = weather.sys.country;
-			$(".location").empty().append(`<p> ${city}, ${country} </p>`);
-
-			const des = weather.weather[0].description;
-			$(".description").empty().append(`<p> ${des} </p>`);
-
-			let windspeed = weather.wind.speed;
-			windspeed = ((windspeed*3600)*3.28/5280).toFixed(0);
-			$(".wind").empty().append(`<p>The wind is blowing at ${windspeed} mph.</p>`);
-			const hum = weather.main.humidity;
-			$(".sky").empty().append("<p>The humidity is "+hum+"%.</p>");
-
-		});
-	}
+				getWeather(newlat, newlog);
+			});
+		} else {
+			$("#zip").val("Please enter a 5-digit number.");
+		}
+		event.preventDefault();
+	});
 });
+
+
 
