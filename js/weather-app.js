02bc24870b1d76668d1258fe62ad5200f1493b9e
Nicholas Engleman <nicholas@englemaninvest.com>
2018-02-21 22:46:14 -0500

    misc bug fixes, add catch-try statement for invalid zip code entries

--- js/weather-app.js
+++ js/weather-app.js
@@ -9,16 +9,10 @@
 $(document).ready(() => {
 	navigator.geolocation.getCurrentPosition(weatherAppController.getCurrentPositionSuccess);
 
-	$("#forecastDay0").on("mouseenter", {value: 0}, weatherAppController.OnMouseEnter);
-	$("#forecastDay1").on("mouseenter", {value: 1}, weatherAppController.OnMouseEnter);
-	$("#forecastDay2").on("mouseenter", {value: 2}, weatherAppController.OnMouseEnter);
-	$("#forecastDay3").on("mouseenter", {value: 3}, weatherAppController.OnMouseEnter);
-
-//	weatherAppModel.updateHourlyForecastData();
-
 	$("#fahrenheit").click(function () {
 		const $this = $(this);
 		if (!$this.hasClass("selected")) {
+			weatherAppView.helperFunctions.stopAnimation();
 			weatherAppModel.temperatureSystemIsF = true;
 			weatherAppModel.dailyForecastDOMData.length = 0;
 			weatherAppModel.hourlyForecastDOMData.length = 0;
@@ -33,6 +27,7 @@ $(document).ready(() => {
 	$("#celsius").click(function () {
 		const $this = $(this);
 		if (!$this.hasClass("selected")) {
+			weatherAppView.helperFunctions.stopAnimation();
 			weatherAppModel.temperatureSystemIsF = false;
 			weatherAppModel.dailyForecastDOMData.length = 0;
 			weatherAppModel.hourlyForecastDOMData.length = 0;
@@ -52,15 +47,20 @@ $(document).ready(() => {
 		// verifies that the input is a 5-digit number
 		if (/^\d{5}$/g.test(zipinput)) {
 			const zipurl = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipinput}`;
+
 			$.getJSON(zipurl, zipdata => {
-				weatherAppModel.latitude = zipdata.results[0].geometry.location.lat;
-				weatherAppModel.longitude = zipdata.results[0].geometry.location.lng;
+				try {
+					weatherAppModel.latitude = zipdata.results[0].geometry.location.lat;
+					weatherAppModel.longitude = zipdata.results[0].geometry.location.lng;
+				} catch (error) {
+					$("#zipcodeInput").val("That is not a zipcode.");
+				}
 
 				weatherAppModel.dailyForecastDOMData.length = 0;
 				weatherAppModel.hourlyForecastDOMData.length = 0;
 
-				weatherAppModel.updateCurrentWeatherData(weatherAppModel.latitude, weatherAppModel.longitude, weatherAppModel.temperatureSystemIsF);
-				weatherAppModel.updateDailyForecastData(weatherAppModel.latitude, weatherAppModel.longitude, weatherAppModel.temperatureSystemIsF);
+				weatherAppModel.updateCurrentWeatherData(weatherAppModel.latitude, weatherAppModel.longitude);
+				weatherAppModel.updateDailyForecastData(weatherAppModel.latitude, weatherAppModel.longitude);
 			});
 		} else {
 			$("#zipcodeInput").val("5-digit number only");
@@ -76,10 +76,10 @@ const weatherAppModel = {
 	temperatureData: {
 		currentTemp: 0,
 		day: [
-			{ Hi: 0, Low: 0, "12:00am": 0, "6:00am" : 0, "12:00pm": 0, "6:00pm" : 0 },
-			{ Hi: 0, Low: 0, "12:00am": 0, "6:00am" : 0, "12:00pm": 0, "6:00pm" : 0 },
-			{ Hi: 0, Low: 0, "12:00am": 0, "6:00am" : 0, "12:00pm": 0, "6:00pm" : 0 },
-			{ Hi: 0, Low: 0, "12:00am": 0, "6:00am" : 0, "12:00pm": 0, "6:00pm" : 0 }
+			{High: 0, Low: 0, "12:00am": 0, "6:00am": 0, "12:00pm": 0, "6:00pm": 0},
+			{High: 0, Low: 0, "12:00am": 0, "6:00am": 0, "12:00pm": 0, "6:00pm": 0},
+			{High: 0, Low: 0, "12:00am": 0, "6:00am": 0, "12:00pm": 0, "6:00pm": 0},
+			{High: 0, Low: 0, "12:00am": 0, "6:00am": 0, "12:00pm": 0, "6:00pm": 0}
 		],
 	},
 	latitude: 0,
@@ -89,7 +89,7 @@ const weatherAppModel = {
 		const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4bd0296ac3468ba55671920cabb0f745`;
 
 		$.getJSON(url, weather => {
-			this.temperatureData.currentTemp = weather.main.temp;
+			this.temperatureData.currentTemp = this.convertTemperature(weather.main.temp, true);
 			const weatherDataObject = {
 				cloudCoverage: weather.clouds.all,
 				description: weather.weather[0].description,
@@ -99,7 +99,7 @@ const weatherAppModel = {
 				humidity: weather.main.humidity,
 				windDirection: this.helperFunctions.computeWindDirection(weather.wind.deg),
 				windspeed: (weather.wind.speed * 3600 * 3.28 / 5280).toFixed(0),
-				visibility: Math.round(weather.visibility * 0.000621371)
+				visibility: Math.round((weather.visibility * 3.28) / 5280)
 			};
 			weatherAppView.displayCurrentWeather(weatherDataObject);
 		});
@@ -107,21 +107,38 @@ const weatherAppModel = {
 	updateDailyForecastData(latitude, longitude) {
 		/*OpenWeatherMap 5-day Forecast API
         the API returns an inconvenient array of fourty 3-hour forecasts. To find weather forecasts for the next four
-        24-hour periods, several functions are used to massage the data before it is displayed.
+        24-hour periods, several functions are used to merge and massage the data before it is displayed.
         */
 		const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=4bd0296ac3468ba55671920cabb0f745`;
 		$.getJSON(url, weatherData => {
 			let delayTime = 0;
-			for (let day = 0; day < 4; day++) {
-				this.temperatureData.day[day].Hi = this.helperFunctions.computeHighTemp(weatherData, day);
-				this.temperatureData.day[day].Low = this.helperFunctions.computeLowTemp(weatherData, day);
-			}
+			// for (let day = 0; day < 4; day++) {
+			// 	this.temperatureData.day[day].High = this.helperFunctions.computeHighTemp(weatherData, day);
+			// 	this.temperatureData.day[day].Low = this.helperFunctions.computeLowTemp(weatherData, day);
+			// }
+			this.temperatureData.day.forEach((e, day) => {
+				e.High = this.helperFunctions.computeHighTemp(weatherData, day);
+				e.Low = this.helperFunctions.computeLowTemp(weatherData, day);
+			});
 
 			for (let day = 0; day < 4; day++) {
 				weatherAppView.displayDailyForecast(weatherData, day, delayTime);
 				delayTime = delayTime + 300;
 			}
 		});
+
+		//sets event handlers approximately when last animation inside above for-loop has finished
+		// setTimeout(function() {
+		// 	$("#forecastDay0").on("mouseenter", {value: 0}, weatherAppController.OnMouseEnter);
+		// 	$("#forecastDay1").on("mouseenter", {value: 1}, weatherAppController.OnMouseEnter);
+		// 	$("#forecastDay2").on("mouseenter", {value: 2}, weatherAppController.OnMouseEnter);
+		// 	$("#forecastDay3").on("mouseenter", {value: 3}, weatherAppController.OnMouseEnter);
+		// }, 1900);
+
+		$("#forecastDay0").on("mouseenter", {value: 0}, weatherAppController.OnMouseEnter);
+		$("#forecastDay1").on("mouseenter", {value: 1}, weatherAppController.OnMouseEnter);
+		$("#forecastDay2").on("mouseenter", {value: 2}, weatherAppController.OnMouseEnter);
+		$("#forecastDay3").on("mouseenter", {value: 3}, weatherAppController.OnMouseEnter);
 	},
 	updateHourlyForecastData() {
 		const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${weatherAppModel.latitude}&lon=${weatherAppModel.longitude}&appid=4bd0296ac3468ba55671920cabb0f745`;
@@ -133,7 +150,7 @@ const weatherAppModel = {
 	findDesiredHourlyForecasts(weatherData) {
 		for (let forecastBlock = 0, day = 0; forecastBlock < weatherData.list.length && day < 4; forecastBlock++) {
 			if (weatherData.list[forecastBlock].dt_txt.includes("06:00:00")) {
-				this.temperatureData.day[day]["12:00am"] = this.convertTemperature(weatherData.list[forecastBlock].main.temp,true);
+				this.temperatureData.day[day]["12:00am"] = this.convertTemperature(weatherData.list[forecastBlock].main.temp, true);
 				weatherAppView.displayHourlyForecast(weatherData.list[forecastBlock], "12:00am", day, 1, true);
 				day++;
 			}
@@ -164,37 +181,42 @@ const weatherAppModel = {
 		}
 	},
 	updateTemperatureData() {
-		this.temperatureData.currentTemp =  this.convertTemperature(this.temperatureData.currentTemp, false);
+		this.temperatureData.currentTemp = this.convertTemperature(this.temperatureData.currentTemp, false);
+		for (let day = 0; day < 4; day++) {
+			this.temperatureData.day[day].High = this.convertTemperature(this.temperatureData.day[day].High, false);
+			this.temperatureData.day[day].Low = this.convertTemperature(this.temperatureData.day[day].Low, false);
+		}
 		weatherAppView.displayConvertedTemperatures();
 	},
 	convertTemperature(temp, fromKelvin) {
-		if(fromKelvin) {
+		if (fromKelvin) {
 			if (weatherAppModel.temperatureSystemIsF) {
-				return Math.round(Math.floor(temp) * (9 / 5) - 459.67);
+				return Math.round((temp) * (9 / 5) - 459.67);
 			} else {
-				return Math.round(Math.floor(temp) - 273.15);
+				return Math.round(temp - 273.15);
 			}
 		} else {
 			if (weatherAppModel.temperatureSystemIsF) {
-				return Math.round((temp - 32) * (5/9));
-			} else {
 				return Math.round((temp * (9 / 5)) + 32);
+			} else {
+				return Math.round((temp - 32) * (5 / 9));
 			}
 		}
 	},
 	helperFunctions: {
 		computeHighTemp(weatherData, day) {
 			let maxTemp = 0;
-			const startDay = day  * 8;
+			const startDay = day * 8;
 			for (let e = startDay; e < startDay + 8; e++) {
 				if (maxTemp < weatherData.list[e].main.temp_max) {
 					maxTemp = weatherData.list[e].main.temp_max;
 				}
 			}
-			if (this.temperatureSystemIsF) {
-				return `${Math.round(maxTemp * (9 / 5) - 459.67)}&#176`;
+			if (weatherAppModel.temperatureSystemIsF) {
+				return Math.round((maxTemp * (9 / 5)) - 459.67);
+			} else {
+				return Math.round(maxTemp - 273.15);
 			}
-			return Math.round(maxTemp - 273.15);
 		},
 		computeLowTemp(weatherData, day) {
 			let minTemp = 1000;
@@ -204,10 +226,11 @@ const weatherAppModel = {
 					minTemp = weatherData.list[e].main.temp_min;
 				}
 			}
-			if (this.temperatureSystemIsF) {
-				return `${Math.round(minTemp * (9 / 5) - 459.67)}&#176`;
+			if (weatherAppModel.temperatureSystemIsF) {
+				return Math.round((minTemp * (9 / 5)) - 459.67);
+			} else {
+				return Math.round(minTemp - 273.15);
 			}
-			return Math.round(minTemp - 273.15);
 		},
 		computeAverageCloudCoverage(weatherData, day) {
 			let totalCloudCoverage = 0;
@@ -258,11 +281,11 @@ const weatherAppModel = {
 			let averageRain = 0;
 			const startDay = day * 8;
 			for (let e = startDay; e < startDay + 8; e++) {
-				if (weatherData.list[e].rain) {
+				if (weatherData.list[e].rain['3h']) {
 					averageRain++;
 				}
 			}
-			return Math.round((averageRain / 8) * 0.7 * 100);
+			return Math.round((averageRain / 8) * 0.8 * 100);
 		},
 		createDescription(weatherData, day) {
 			let clouds, rain;
@@ -282,7 +305,6 @@ const weatherAppModel = {
 			} else {
 				clouds = "overcast";
 			}
-
 			if (averageRain === 0) {
 				rain = "with no rain";
 			} else if (averageRain < 15) {
@@ -296,7 +318,6 @@ const weatherAppModel = {
 			} else {
 				rain = "with lots of rain";
 			}
-
 			return `${clouds} ${rain}`;
 		}
 	}
@@ -307,7 +328,7 @@ const weatherAppView = {
 	displayCurrentWeather(weatherDataObject) {
 		$("#currentTemperature")
 			.empty()
-			.append(`<p> ${weatherAppModel.convertTemperature(weatherAppModel.temperatureData.currentTemp, true)}&#176</p>`);
+			.append(`<p> ${weatherAppModel.temperatureData.currentTemp}&#176</p>`);
 		$("#currentDescription")
 			.empty()
 			.append(`<p>"${weatherDataObject.description}"</p>`);
@@ -351,13 +372,13 @@ const weatherAppView = {
 		$(`#iconForecast${day}`)
 			.css("opacity", 0)
 			.empty()
-			.append(`<img src='icons/${this.helperFunctions.getDailyForecastIcon(weatherAppModel.helperFunctions.computeAverageCloudCoverage(weatherData, day), true)}'>`)
+			.append(`<img src='icons/${this.helperFunctions.getDailyForecastIcon(weatherAppModel.helperFunctions.computeAverageCloudCoverage(weatherData, day), weatherAppModel.helperFunctions.computeAverageRain(weatherData, day), true)}'>`)
 			.delay(delayTime)
 			.animate({opacity: 1}, animationDelay);
 		$(`#maxTemp${day}`)
 			.css("opacity", 0)
 			.empty()
-			.append(`<p>${weatherAppModel.temperatureData.day[day].Hi}&#176</p>`)
+			.append(`<p>${weatherAppModel.temperatureData.day[day].High}&#176</p>`)
 			.delay(delayTime)
 			.animate({opacity: 1}, animationDelay);
 		$(`#minTemp${day}`)
@@ -403,10 +424,16 @@ const weatherAppView = {
 			.empty()
 			.append(`<p> ${weatherAppModel.temperatureData.currentTemp}&#176</p>`)
 			.animate({opacity: 1}, 2000);
-		// $(`#maxTemp${day}`).css("opacity", 0).empty().append(`<p>${weatherAppModel.helperFunctions.computeHighTemp(weatherForecast, day, weatherAppModel.temperatureSystemIsF)}</p>`)
-		// 	.animate({opacity: 1}, 2000);
-		// $(`#minTemp${day}`).css("opacity", 0).empty().append(`<p>${weatherAppModel.helperFunctions.computeLowTemp(weatherForecast, day, weatherAppModel.temperatureSystemIsF)}</p>`)
-		// 	.animate({opacity: 1}, 2000);
+		for (let day = 0; day < 4; day++) {
+			$(`#maxTemp${day}`)
+				.css("opacity", 0)
+				.empty().append(`<p>${weatherAppModel.temperatureData.day[day].High}&#176</p>`)
+				.animate({opacity: 1}, 2000);
+			$(`#minTemp${day}`)
+				.css("opacity", 0).empty()
+				.append(`<p>${weatherAppModel.temperatureData.day[day].Low}&#176</p>`)
+				.animate({opacity: 1}, 2000);
+		}
 	},
 	helperFunctions: {
 		updateForecastDates(increment) {
@@ -435,14 +462,24 @@ const weatherAppView = {
 			}
 		},
 		stopAnimation(day, callback) {
-			$(`#date${day}`).finish();
-			$(`#iconForecast${day}`).finish();
-			$(`#maxTemp${day}`).finish();
-			$(`#minTemp${day}`).finish();
-			$(`#description${day}`).finish();
-			$(`#windForecast${day}`).finish();
-			$(`#rainForecast${day}`).finish();
-			callback();
+			if (day) {
+				$(`#date${day}`).finish();
+				$(`#iconForecast${day}`).finish();
+				$(`#maxTemp${day}`).finish();
+				$(`#minTemp${day}`).finish();
+				$(`#description${day}`).finish();
+				$(`#windForecast${day}`).finish();
+				$(`#rainForecast${day}`).finish();
+			} else {
+				$('#currentTemperature').finish();
+				for (let day = 0; day < 4; day++) {
+					$(`#maxTemp${day}`).finish();
+					$(`#minTemp${day}`).finish();
+				}
+			}
+			if (callback) {
+				callback();
+			}
 		},
 		printHourlyForecastDOMTemplate(day) {
 			return `
@@ -469,43 +506,69 @@ const weatherAppView = {
 					</div>
 				</div>`;
 		},
-		getDailyForecastIcon(cloudCoverage, forecastYesNo) {
+		getDailyForecastIcon(cloudCoverage, averageRain, forecastYesNo) {
 			const timeNow = new Date(Date.now());
 			//	let timeOfSunrise = new Date(timeNow).setHours(0, 0, 0, 0) + new Date(apiSunriseTime).setDate(1) - 18000000;
 			const timeOfSunrise = new Date(timeNow).setHours(6, 30, 0, 0);
 			const timeOfSunset = new Date(timeNow).setHours(19, 30, 0, 0);
 
 			if ((timeNow < timeOfSunrise || timeOfSunset < timeNow) && !forecastYesNo) {
-				if (cloudCoverage < 10) {
-					return "night.svg";
+				if (averageRain < 10) {
+					if (cloudCoverage < 10) {
+						return "night.svg";
+					}
+					if (cloudCoverage < 30) {
+						return "cloudy-night-1.svg";
+					}
+					if (cloudCoverage < 60) {
+						return "cloudy-night-2.svg";
+					}
+					if (cloudCoverage < 75) {
+						return "cloudy-night-3.svg";
+					}
+					if (cloudCoverage) {
+						return "cloudy.svg";
+					}
+				} else {
+					if (averageRain < 30) {
+						return "rainy-4.svg";
+					}
+					if (averageRain < 60) {
+						return "rainy-5.svg"
+					}
+					if (averageRain) {
+						return "rainy-6.svg"
+					}
 				}
-				if (cloudCoverage < 30) {
-					return "cloudy-night-1.svg";
+			}
+			else {
+				if (averageRain < 10) {
+					if (cloudCoverage < 10) {
+						return "day.svg";
+					}
+					if (cloudCoverage < 30) {
+						return "cloudy-day-1.svg";
+					}
+					if (cloudCoverage < 60) {
+						return "cloudy-day-2.svg";
+					}
+					if (cloudCoverage < 75) {
+						return "cloudy-day-3.svg";
+					}
+					if (cloudCoverage) {
+						return "cloudy.svg";
+					}
 				}
-				if (cloudCoverage < 60) {
-					return "cloudy-night-2.svg";
-				}
-				if (cloudCoverage < 75) {
-					return "cloudy-night-3.svg";
-				}
-				if (cloudCoverage < 101) {
-					return "cloudy.svg";
-				}
-			} else {
-				if (cloudCoverage < 10) {
-					return "day.svg";
-				}
-				if (cloudCoverage < 30) {
-					return "cloudy-day-1.svg";
-				}
-				if (cloudCoverage < 60) {
-					return "cloudy-day-2.svg";
-				}
-				if (cloudCoverage < 75) {
-					return "cloudy-day-3.svg";
-				}
-				if (cloudCoverage < 101) {
-					return "cloudy.svg";
+				else {
+					if (averageRain < 30) {
+						return "rainy-2.svg";
+					}
+					if (averageRain < 60) {
+						return "rainy-1.svg"
+					}
+					if (averageRain) {
+						return "rainy-3.svg"
+					}
 				}
 			}
 		},
@@ -571,21 +634,22 @@ const weatherAppController = {
 		weatherAppModel.latitude = position.coords.latitude;
 		weatherAppModel.longitude = position.coords.longitude;
 
-		weatherAppModel.updateCurrentWeatherData(weatherAppModel.latitude, weatherAppModel.longitude, weatherAppModel.temperatureSystemIsF);
-		weatherAppModel.updateDailyForecastData(weatherAppModel.latitude, weatherAppModel.longitude, weatherAppModel.temperatureSystemIsF);
+		weatherAppModel.updateCurrentWeatherData(weatherAppModel.latitude, weatherAppModel.longitude);
+		weatherAppModel.updateDailyForecastData(weatherAppModel.latitude, weatherAppModel.longitude, function() {
+
+		});
 	},
 	OnMouseEnter(event) {
 		const $forecastDay = $(`#forecastDay${event.data.value}`);
 		$(".redButton").hide();
 		$forecastDay.off("mouseenter");
 
-		if (!weatherAppModel.dailyForecastDOMData[event.data.value] && $(`#rainForecast3`).text() !== "") {
+		if (!weatherAppModel.dailyForecastDOMData[event.data.value]) {
 			weatherAppView.helperFunctions.stopAnimation(event.data.value, function () {
 				weatherAppModel.dailyForecastDOMData[event.data.value] = $forecastDay.html();
 			});
 		}
 		if (!weatherAppModel.hourlyForecastDOMData[event.data.value]) {
-
 			$forecastDay.empty().append(weatherAppView.helperFunctions.printHourlyForecastDOMTemplate(event.data.value));
 			weatherAppModel.updateHourlyForecastData();
 		} else {
@@ -597,7 +661,7 @@ const weatherAppController = {
 		const $forecastDay = $(`#forecastDay${event.data.value}`);
 		$forecastDay.off("mouseleave");
 
-		if (!weatherAppModel.hourlyForecastDOMData[event.data.value] && $(`#forecastDetailTemp${event.data.value}4`).text() !== "") {
+		if (!weatherAppModel.hourlyForecastDOMData[event.data.value]) {
 			weatherAppModel.hourlyForecastDOMData[event.data.value] = $forecastDay.html();
 		}
 		$forecastDay.empty().append(weatherAppModel.dailyForecastDOMData[event.data.value]);
